a198051da594ab474d1c77f8d209e87bb5edde14
Ruslan Karpov <rkarpov88@gmail.com>
2022-11-22 12:56:29 +0200

    finished task 01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -14,7 +14,7 @@ function createGalleryItemsMarkup(galleryItems) {
     .map(({ preview, original, description }) => {
         return `
         <div class="gallery__item">
-        <a class="gallery__link" href="${original.value}">
+        <a class="gallery__link" href="${original}">
          <img
             class="gallery__image"
             src="${preview}"
