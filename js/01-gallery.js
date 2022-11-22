0782162040ad1357c0f9f785d5e7257e85f4610a
Ruslan Karpov <rkarpov88@gmail.com>
2022-11-22 12:20:30 +0200

    01-gallery 1st list end

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const imageContainer = document.querySelector('.gallery');
+const cardsMarkup = createGalleryItemsMarkup(galleryItems);
+
+imageContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+imageContainer.addEventListener('click', onImageCardClick);
+
+function createGalleryItemsMarkup(galleryItems) {
+    return galleryItems  
+
+    .map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+         <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+        />
+        </a>
+        </div>`;
+    })
+        .join('');
+}
+
+function onImageCardClick(event) {
+    if (!event.target.classList.contains('gallery__item')) {
+        return;
+    }
+     console.log(event.target)
+}
+
+
